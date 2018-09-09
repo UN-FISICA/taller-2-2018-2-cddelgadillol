a99147d03403d554d98ff9720a20e18ca95998dd
cddelgadillol <42813059+cddelgadillol@users.noreply.github.com>
2018-09-09 00:11:12 -0500

    Add files via upload
    ejercicio 5 terminado

---
+++ Ejercicio5.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8-*-
+import turtle
+from turtle import *
+title("ejer4")
+penup()
+goto(-300,-200)
+y=int(input('Escriba el numero de pisos de la piramide:'))
+def poligono():
+	g=0
+	pendown()
+	while (g<C):
+		turtle.forward(20)
+		turtle.left(180-(180/C)*(C-2))
+		g=g+1
+	penup()
+	turtle.forward(70)
+A=0
+B=0
+C=0
+while A<y:
+	B=A
+	C=2+y-A
+	while B<y:
+	  poligono()
+	  B=B+1
+	A=A+1
+	goto(-300+(35)*A,-200+70*A)
+wait = input("PREsione enter.")
